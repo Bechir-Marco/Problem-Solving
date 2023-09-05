b14381dac9dd815955d6f9b8b1c41233014dc711
Bechir-Marco <bechir.marko@gmail.com>
2023-09-05 16:51:34 +0100

    slight improvement

--- Min Diff Highest and Lowest of K Scores(#Sliding Window)/index.js
+++ Min Diff Highest and Lowest of K Scores(#Sliding Window)/index.js
@@ -1,18 +1,15 @@
 var minimumDifference = function (nums, k) {
-    if (nums.length <= k) {
+    if (nums.length < k) {
         return 0;
     }
 
     nums.sort((a, b) => a - b);
     let lowest = Infinity;
-    let diff = 0;
 
     for (let i = 0; i <= nums.length - k; i++) {
-        diff = nums[i + k - 1] - nums[i];
-        lowest = Math.min(lowest, diff);
+        const diff = nums[i + k - 1] - nums[i];
+        lowest = Math.min(diff, lowest);
     }
 
     return lowest;
 };
-
-
